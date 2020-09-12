a302cfeba6602b306cf272c6d46a494925238304
skj <sujan@innovatetech.co>
2020-09-12 12:09:08 +0545

    Initialize with base react app

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+module.exports = {
+  // define entry file and output
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index.js',
+  },
+  // define babel loader
+  module: {
+    rules: [
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+    ],
+  },
+  devServer: {
+    port: 3000,
+  },
+};
